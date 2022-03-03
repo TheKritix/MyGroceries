20b85fd0f70389958c8d665bc7240998d43c2e66
KRTP Project <KRTP@rambolldminsmbp.students.clients.local>
2022-03-03 12:48:02 +0100

    Initial commit

---
+++ MyGroceries/MyGroceriesApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyGroceriesApp.swift
+//  MyGroceries
+//
+//  Created by KRTP Project on 03/03/2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct MyGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
