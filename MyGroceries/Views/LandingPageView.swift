42934be6fd4058789e459886ad1be62f23ab61e3
Mia Dong s205353 <Mia Dong s205353>
2022-04-01 22:30:38 +0200

    MyGroceries landingpage

--- MyGroceries/Views/LandingPageView.swift
+++ MyGroceries/Views/LandingPageView.swift
@@ -24,11 +24,33 @@ struct LandingPageView : View {
                         .background(.white)
                         .cornerRadius(15)
                 ZStack {
-                    RoundedRectangle(cornerRadius: 16.0)
-                        .fill(.teal)
+                    RoundedRectangle(cornerRadius: 30.0)
+                        .fill(.white)
                         .opacity(0.4)
                 }
-                .frame(width: 360)
+                .frame(width: 360, height: 200)
+                ZStack {
+                    Button(action: {
+                        
+                    }){
+                        Text("THIS IS A BUTTON \n YES IT IS")
+                    }
+                    .background(.white)
+                    
+                    RoundedRectangle(cornerRadius: 30.0)
+                        .fill(.white)
+                        .opacity(0.4)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30.0)
+                            .fill(.black)
+                            .opacity(0.2)
+
+                    }.frame(width: 10, height: 550)
+                }
+                .frame(width: 360, height: 550)
+                .padding()
+
             }
         }
         
